062b4433de900ac82d856ec48b3ca6405f83d75c
Gabriel1470 <gabriel1470bg@gmail.com>
2023-06-23 17:14:48 +0200

    text maanipulation forse

---
+++ textManipulation.cs
@@ -0,0 +1,12 @@
+﻿string Text, Revers = "";
+int Length;
+Console.Write("Scrivi quel che vuoi : ");
+Text = Console.ReadLine();
+Length = Text.Length - 1;
+while (Length >= 0)
+{
+Revers = Revers + Text[Length];
+Length--;
+}
+Console.WriteLine("Capovolgo il suo testo  {0}", Revers);
+Console.ReadLine();
