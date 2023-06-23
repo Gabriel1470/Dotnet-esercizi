9ff8eefb7592acec393c7703c14fff90da02726f
Gabriel1470 <gabriel1470bg@gmail.com>
2023-06-23 16:15:49 +0200

    Esercizio 2 Now it's personal

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, what is your name!");
+
+string name = Console.ReadLine();
+string saluta = name.Trim();
+
+Console.WriteLine("Hello, " + saluta);
