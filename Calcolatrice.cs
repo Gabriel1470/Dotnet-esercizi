c22af8ff523dac078662f823237008ac3f1d03a1
Gabriel1470 <gabriel1470bg@gmail.com>
2023-06-23 16:37:25 +0200

    esercizio The match checks out

---
+++ Calcolatrice.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Primo numero:");
+
+string textA =Console.ReadLine();
+int numberA = int.Parse(textA);
+
+Console.WriteLine("Secondo numero:");
+string textB =Console.ReadLine();
+int numberB = int.Parse(textB);
+
+int result = numberA + numberB;
+Console.Write($"Il risultato di {numberA} + {numberB} = ");
+Console.WriteLine(result); 
