86f5a85b7f26771505b2a99ee03467b06d3f1090
Gabriel1470 <gabriel1470bg@gmail.com>
2023-06-24 16:17:12 +0200

    convertitore da celsius a farenheit

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Inserire la temperatura in Celsius : ");
+double celsius = Convert.ToDouble(Console.ReadLine());
+double fahrenheit = ((celsius * 1.8) + 32 ) ;
+Console.WriteLine("La temperatura in Fahrenheit : " + fahrenheit);
