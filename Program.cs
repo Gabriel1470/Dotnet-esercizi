7e62b419c61bede0ad5a688c5987615c6d6de96a
Gabriel1470 <gabriel1470bg@gmail.com>
2023-06-24 15:37:15 +0200

    Palindromo esercizio 8

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Inserisci una parola o una frase");
+string? input = Console.ReadLine();
+
+if (input == null || input == "")
+{
+    Console.WriteLine("Input non valido");
+    Environment.Exit(1);
+    return;
+}
+
+input = input.Replace(" ", "").ToLower();
+string reverse = new(input.Reverse().ToArray());
+
+if (input.Equals(reverse))
+{
+    Console.WriteLine("E un palindromo");
+}
+else
+{
+    Console.WriteLine("Non e un  palindromo");
+}
