0ed20f5be59a4e39ece76cf90e51f4ee9a118793
Gabriel1470 <gabriel1470bg@gmail.com>
2023-06-24 15:25:15 +0200

    Acces denied credo

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿MyClass myObj = new MyClass();
+
+//Console.WriteLine(myObj.privateMember);
+
+//Console.WriteLine(myObj.protectedMember);
+
+Console.WriteLine(myObj.internalMember);
+
+Console.WriteLine(myObj.publicMember);
+
+
+
+public class MyClass
+{
+    private int privateMember = 10;
+    protected int protectedMember = 20;
+    internal int internalMember = 30;
+    public int publicMember = 40;
+}
